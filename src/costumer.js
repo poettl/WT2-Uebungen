7e90921569504d312cafc810123ea2c067ef6f1a
fabian-weng <58660953+fabian-weng@users.noreply.github.com>
2020-04-17 13:46:42 +0200

    Create costumer.js

---
+++ src/costumer.js
@@ -0,0 +1,10 @@
+class costumer{
+    constructor(name, adress, telnumber, email){
+        this.name = name;
+        this.adress = adress;
+        this.telnumber = telnumber;
+        this.email = email;
+    }
+}
+
+export default costumer;
