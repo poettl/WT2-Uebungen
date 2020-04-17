05d31d7e448e5034d97e9abc5ef747d76877bc2e
czeflo <58661381+czeflo@users.noreply.github.com>
2020-04-17 11:38:55 +0200

    bareboned class product

---
+++ src/products.js
@@ -0,0 +1,10 @@
+class product {
+    constructor(name, price, pricePerWeek, pricePerMonth){
+        this.name = name;
+        this.price = price;
+        this.pricePerWeek = pricePerWeek;
+        this.pricePerMonth = pricePerMonth;
+    }
+}
+
+export default product;
