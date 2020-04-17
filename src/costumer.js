8ee554a8c9a9d1d96cda71593a57ff03e47b7fd9
fabian-weng <58660953+fabian-weng@users.noreply.github.com>
2020-04-17 14:05:53 +0200

    Update costumer.js

--- src/costumer.js
+++ src/costumer.js
@@ -5,6 +5,38 @@ class costumer{
         this.telnumber = telnumber;
         this.email = email;
     }
+
+    get name(){
+        return this.name;
+    }
+
+    get adress(){
+        return this.adress;
+    }
+
+    get telnumber(){
+        return this.telnumber;
+    }
+
+    get email(){
+        return this.email;
+    }
+
+    set name(name){
+        this.name=name;
+    }
+
+    set adress(adress){
+        this.adress=adress;
+    }
+
+    set telnumber(telnumber){
+        this.telnumber=telnumber;
+    }
+
+    set email(email){
+        this.email=email;
+    }
 }
 
 export default costumer;
