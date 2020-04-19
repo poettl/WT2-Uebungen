0602e447537ccddb44fb38f1342c438206937f65
czeflo <58661381+czeflo@users.noreply.github.com>
2020-04-19 10:38:44 +0200

    Update product.js

--- src/product.js
+++ src/product.js
@@ -1,9 +1,32 @@
 class product{
-    constructor(name, price){
-        this.name = name;
-        this.price = price;
-        this.pricePerWeek = 0;
-        this.pricePerMonth = 0;
+    constructor(name, price, massDiscount){
+        this._name = name;
+        this._price = price;
+        this._massDiscount = massDiscount
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+
+    get price(){
+        return this._price;
+    }
+
+    set price(price){
+        this._price = price;
+    }
+
+    get pricePerWeek(){
+        return this._price * 7 * (1-_massDiscount); //applies the mass discount 
+    }
+
+    get pricePerMonth(){
+        return this._price * 30 * (1-_massDiscount*2) //doubles the mass discount for the month
     }
 }
 
