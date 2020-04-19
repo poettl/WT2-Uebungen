3c22c6247aedf33958ba8b51e89752c5ccc6d3d6
Florian Czeczil <58661381+czeflo@users.noreply.github.com>
2020-04-19 15:12:01 +0200

    changed product class and added some new tests

--- src/product.js
+++ src/product.js
@@ -1,16 +1,18 @@
 class product{
-    constructor(name, price, massDiscount){
+    constructor(name, price, massDiscount, tax){
         this._name = name;
         this._price = price;
         this._massDiscount = massDiscount
+        this._tax = tax;
     }
-
     get name(){
         return this._name;
     }
 
     set name(name){
-        this._name = name;
+        if(name != null && name != ""){
+            this._name = name;
+        }
     }
 
     get price(){
@@ -18,15 +20,21 @@ class product{
     }
 
     set price(price){
-        this._price = price;
+        if(price != null && price != 0){
+            this._price = price;
+        }
     }
 
     get pricePerDay(){
-        return this._price * 24 * (1-this._massDiscount); //applies the mass discount, if it's booked for a day
+        return this._price * 24 * (1-this._massDiscount) * (1 + this._tax); //applies the mass discount, if it's booked for a day, including taxes
     }
 
     get pricePerWeek(){
-        return this._price * 24 * 7 * (1-this._massDiscount*2) //doubles the mass discount for the week
+        return this._price * 24 * 7 * (1-this._massDiscount*2) * (1 + this._tax) //doubles the mass discount for the week, including taxes
+    }
+
+    get taxAmount(){
+        return this._price * this._tax; //returns the amout of taxes that are put on top of the prices
     }
 }
 
--- test/product.test.js
+++ test/product.test.js
@@ -1,17 +1,51 @@
 import Product from '../src/product'
+import { IgnorePlugin } from 'webpack';
 var assert = require('assert')
 
 const testProduct = () => {
-    describe('Product', function () {
-        let testProduct = new Product("bagger", 100, 0.05)
-        describe('#getPricePerDay', function () {
-            it('should return the right price per day for a set price', function () {
-                assert.equal(testProduct.pricePerDay, 2280) //then it calculated the price right
+    describe('Product', () => {
+        let testProduct = new Product("bagger", 100, 0.05, 0.2) //5% Discount, 20% Tax in Austria
+        describe('#getPricePerDay', () => {
+            it('should calc and return the right price per day for a set hourly price, including taxes', () => {
+                assert.equal(testProduct.pricePerDay, 2736); //then it calculated the price right
             });
         });
-        describe('#getPricePerWeek', function(){
-            it('should return the right price per week for a set price', function(){
-                assert.equal(testProduct.pricePerWeek, 15120) //then it calculated the price right
+        describe('#getPricePerWeek', () => {
+            it('should calc and return the right price per week for a set hourly price, including taxes', () => {
+                assert.equal(testProduct.pricePerWeek, 18144); //then it calculated the price right
+            })
+        })
+        describe('#setName', () => {
+            it('should change the name of the product', () => {
+                testProduct.name = "lkw";
+                assert.equal(testProduct.name, "lkw");
+            });
+            it('should not change the name if the value passed in is null', () => {
+                testProduct.name = null;
+                assert.notEqual(testProduct, null) //if it is not null, the filter worked
+            })
+            it('should not change the name if an empty string is passed', () => {
+                testProduct.name = "";
+                assert.notEqual(testProduct, ""); //if it is not set to the empty string, the filter worked
+            });
+        });
+        describe('#setPrice', () => {
+            it('should change the price of the product', () => {
+                testProduct.price = 200;
+                assert.equal(testProduct.price, 200);
+            });
+            it('should not change the price of the product if null is passed', () => {
+                testProduct.price = null;
+                assert.notEqual(testProduct.price, null);
+            });
+            it('should not change the price of the product if 0 is passed', () => {
+                testProduct.price = 0;
+                assert.notEqual(testProduct.price, 0);
+            });
+        })
+        describe('#getTaxAmount', function(){
+            it('should display the taxes that need to be payed in addition to the price', () => {
+                assert.equal(testProduct.taxAmount, 40) // 20% of 200, which was set before
             })
         })
     });
