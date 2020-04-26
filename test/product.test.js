2ce7c511579c937baecd1192fe6b55e11cec6cbb
czeflo <58661381+czeflo@users.noreply.github.com>
2020-04-26 09:54:23 +0200

    Update product.test.js

--- test/product.test.js
+++ test/product.test.js
@@ -4,13 +4,13 @@ var assert = require('assert')
 const testProduct = () => {
     describe('Product', function () {
         let testProduct = new Product("bagger", 100, 0.05)
-        describe('#getPricePerDay', function () {
-            it('should return the right price per day for a set price', function () {
+        describe('#getPricePerDay()', function () {
+            it('should calculate and return the right price per day for a set price', function () {
                 assert.equal(testProduct.pricePerDay, 2280) //then it calculated the price right
             });
         });
-        describe('#getPricePerWeek', function(){
-            it('should return the right price per week for a set price', function(){
+        describe('#getPricePerWeek()', function(){
+            it('should calculate and return the right price per week for a set price', function(){
                 assert.equal(testProduct.pricePerWeek, 15120) //then it calculated the price right
             })
         })
