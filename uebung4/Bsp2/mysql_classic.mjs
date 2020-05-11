76094ed8e2b5ec6dcb08c5f531b7de9334eb69b3
Peter Öttl <peter.oettl@allaboutapps.at>
2020-05-11 20:58:08 +0200

    update mysql

--- uebung4/Bsp2/mysql_classic.mjs
+++ uebung4/Bsp2/mysql_classic.mjs
@@ -24,7 +24,8 @@ const connection = mysql.createConnection({
   user: 'root',
   password: 'root',
 });
-function runSQLCommand(connection, sql) {
+
+function runSQLCommand(sql) {
   connection.query(sql, function (err, result) {
     if (err) {
       console.log(err);
@@ -34,20 +35,40 @@ function runSQLCommand(connection, sql) {
     }
   });
 }
+
+function getSQLDate(date) {
+  return date.toISOString().slice(0, 19).replace('T', ' ');
+}
+
+function getSQLStringForInsert(dn1) {
+  return `${dn1.customer},${dn1.price},${dn1.count},${dn1.paid},'${getSQLDate(
+    dn1.date
+  )}',${dn1.product}`;
+}
+
 connection.connect(function (err) {
   if (err) throw err;
   console.log('Connected!');
-  let dn1 = new Deliverynotes(1, 10.0, 1, false, new Date(), 1);
-
-  var sqlInsert = `INSERT INTO test.deliverynotes (customer, price, count, date, product) VALUES (${
-    dn1.customer
-  },${dn1.price},${dn1.count},'${dn1.date
-    .toISOString()
-    .slice(0, 19)
-    .replace('T', ' ')}',${dn1.product})`;
-  runSQLCommand(connection, sqlInsert);
-
-  var sqlSelect = 'SELECT * FROM test.deliverynotes';
-  runSQLCommand(connection, sqlSelect);
+  var sqlCreateDatabase = 'CREATE DATABASE IF NOT EXISTS wt;';
+  var sqlCreateTable =
+    'CREATE TABLE IF NOT EXISTS wt.deliverynotes (customer INTEGER, price DECIMAL, count INTEGER, paid BOOL, date DATE, product INTEGER)';
+  runSQLCommand(sqlCreateDatabase);
+  runSQLCommand(sqlCreateTable);
+  let dn1 = new Deliverynotes(1, 15.0, 1, true, new Date(), 2);
+  let dn2 = new Deliverynotes(2, 14.0, 3, false, new Date(), 3);
+  let dn3 = new Deliverynotes(3, 13.0, 4, true, new Date(), 1);
+  let dn4 = new Deliverynotes(4, 12.0, 5, false, new Date(), 4);
+  let dn5 = new Deliverynotes(5, 14.0, 6, true, new Date(), 7);
+  var sqlSelect = 'SELECT * FROM wt.deliverynotes';
+  var sqlInsert = `INSERT INTO wt.deliverynotes (customer, price, count, paid, date, product) VALUES (${getSQLStringForInsert(
+    dn1
+  )}),(${getSQLStringForInsert(dn2)}),(${getSQLStringForInsert(
+    dn3
+  )}),(${getSQLStringForInsert(dn4)}),(${getSQLStringForInsert(
+    dn1
+  )}),(${getSQLStringForInsert(dn5)})`;
+  console.log(sqlInsert);
+  runSQLCommand(sqlInsert);
+  runSQLCommand(sqlSelect);
   connection.end();
 });
--- uebung4/Bsp2/mysql_sequelize.mjs
+++ uebung4/Bsp2/mysql_sequelize.mjs
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-const sequelize = new Sequelize('test', 'root', 'root', {
+const sequelize = new Sequelize('wt', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql',
 });
@@ -12,18 +12,53 @@ var deliverynotesSequelize = sequelize.define('ormtable', {
   paid: Sequelize.BOOLEAN,
   date: Sequelize.DATE,
 });
+var data = [
+  {
+    customer: 2,
+    price: 5,
+    product: 3,
+    count: 1,
+    paid: true,
+    date: new Date(),
+  },
+  {
+    customer: 1,
+    price: 10,
+    product: 1,
+    count: 5,
+    paid: false,
+    date: new Date(),
+  },
+  {
+    customer: 10,
+    price: 5,
+    product: 2,
+    count: 5,
+    paid: false,
+    date: new Date(),
+  },
+  {
+    customer: 1,
+    price: 5,
+    product: 1,
+    count: 1,
+    paid: false,
+    date: new Date(),
+  },
+  {
+    customer: 1,
+    price: 17,
+    product: 1,
+    count: 3,
+    paid: true,
+    date: new Date(),
+  },
+];
 
 sequelize
   .sync()
   .then(function () {
-    return deliverynotesSequelize.create({
-      customer: 1,
-      price: 10,
-      product: 5,
-      count: 8,
-      paid: true,
-      date: new Date(),
-    });
+    return deliverynotesSequelize.bulkCreate(data);
   })
   .then(function (result) {
     sequelize
